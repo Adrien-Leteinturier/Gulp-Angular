24c50ab6dea282d00e18220e95dd337ba31795f5
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2017-06-12 18:20:33 +0200

    add log verify active scrollElement

--- src/app/components/displayEffectService/displayeffect.service.js
+++ src/app/components/displayEffectService/displayeffect.service.js
@@ -13,6 +13,7 @@ angular.module('app')
         angular.element($window).scroll(function(){
           var bottom_of_object =  angular.element(scrollElement).offset().top +  angular.element(scrollElement).outerHeight();
           var bottom_of_window =  angular.element(window).scrollTop() +  angular.element(window).height();
+          $log.info(angular.element(scrollElement))
           if( bottom_of_window > bottom_of_object ){
             angular.element(element).addClass(effect);
             angular.element(element).css({display:'block'});
@@ -21,7 +22,6 @@ angular.module('app')
       }
       this.hoverDisplayEffect = function(hoverElement,element,effect){
         angular.element(hoverElement).mouseenter(function(){
-          $log.info('hover')
           angular.element(element).addClass(effect);
         });        
       }
