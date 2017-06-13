719238c75ae39c39433c7679a43f17c62f1f3cd7
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2017-06-13 09:40:49 +0200

    add scrollsmooth Service in components app

---
+++ src/app/components/scrollSmoothService/scrollsmooth.service.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name oostaooWebsiteApp.scrollSmoothService
+ * @description
+ * # scrollSmoothService
+ * Service in the oostaooWebsiteApp.
+ */
+angular.module('app')
+  .service('scrollSmoothService', function ($log) {
+
+      this.scrollTo = function(element){
+        var offset = angular.element(element).offset().top 
+        angular.element('html, body').animate({scrollTop: offset}, 'slow'); 
+        $log.info(element);
+      }
+
+});
+    
+
