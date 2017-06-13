a376520a197f71bcb4990738fc89491e22d56e95
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2017-06-13 18:13:19 +0200

    delete $log

--- src/app/components/displayEffectService/displayeffect.service.js
+++ src/app/components/displayEffectService/displayeffect.service.js
@@ -8,12 +8,11 @@
  * Service in the appApp.
  */
 angular.module('app')
-  .service('displayEffectService', function ($window,$log) {
+  .service('displayEffectService', function ($window) {
       this.displayEffect = function(scrollElement,element,effect){
         angular.element($window).scroll(function(){
           var bottom_of_object =  angular.element(scrollElement).offset().top +  angular.element(scrollElement).outerHeight();
           var bottom_of_window =  angular.element(window).scrollTop() +  angular.element(window).height();
-          $log.info(angular.element(scrollElement))
           if( bottom_of_window > bottom_of_object ){
             angular.element(element).addClass(effect);
             angular.element(element).css({display:'block'});
