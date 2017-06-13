dbe18ba689b4a83473fadcea8c47641cff254258
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2017-06-13 09:38:50 +0200

    add displayEffectService in footer directive

--- src/app/components/footerDirective/footer.directive.js
+++ src/app/components/footerDirective/footer.directive.js
@@ -7,10 +7,18 @@
  * # footerDirective
  */
 angular.module('app')
-  .directive('footerDirective', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'app/components/footerDirective/footer.html'
-    };
-  });
+  .directive('footerDirective',function(displayEffectService){
+      return {
+        restrict: 'E',
+        templateUrl: 'app/components/footerDirective/footer.html',
+        link: function(){
+        },
+        controller: function($scope) { 
+          $scope.displayEffectFooter = function(scrollElement,element,effect) {
+            displayEffectService.displayEffect(scrollElement,element,effect);
+          }          
+        }
+      };
+    });
+
 
