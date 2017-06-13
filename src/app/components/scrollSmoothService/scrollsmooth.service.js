6037fe3334f918088f113636cd1fa87f6fc83d99
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2017-06-13 18:14:35 +0200

    delete $log

--- src/app/components/scrollSmoothService/scrollsmooth.service.js
+++ src/app/components/scrollSmoothService/scrollsmooth.service.js
@@ -8,12 +8,11 @@
  * Service in the oostaooWebsiteApp.
  */
 angular.module('app')
-  .service('scrollSmoothService', function ($log) {
+  .service('scrollSmoothService', function () {
 
       this.scrollTo = function(element){
         var offset = angular.element(element).offset().top 
         angular.element('html, body').animate({scrollTop: offset}, 'slow'); 
-        $log.info(element);
       }
 
 });
