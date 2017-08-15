20f08aeb09813491f05745fff635ebd59d05e91f
Adrien Leteinturier <adrienleteinturier@yahoo.fr>
2017-08-15 12:01:44 +0200

    modify directive for add data in scope

--- src/app/components/partnersDirective/partners.directive.js
+++ src/app/components/partnersDirective/partners.directive.js
@@ -1,3 +1,4 @@
+(function() {
 'use strict';
 
 /**
@@ -6,14 +7,44 @@
  * @description
  * # partnersDirective
  */
-angular.module('app')
-  .directive('partnersDirective',function(){
-      return {
+    angular
+    .module('app')
+    .directive('partnersDirective', partnersDirective);
+  
+    /** @ngInject */
+    function partnersDirective() {
+      var directive = {
         restrict: 'E',
         templateUrl: 'app/components/partnersDirective/partners.html',
-        link: function(){
-        }
+        controller: PartnersController,
+        controllerAs: 'vm',
+        bindToController: true  
       };
-    });
+  
+      return directive;      
+
+    /** @ngInject */
+    function PartnersController($scope) {
+        $scope.data = [
+          {
+            logoH: 'assets/images/huguenin-logo.png',
+            descriptionH: 'HUGUENIN C2I Spécialiste de l\'immobilier en Ile-de-france',
+            descriptionContentH: '',
+            link1H: '',
+            link2H: '',
+            link3H: '',
+            imgLink1H: 'huguenin-facebook.jpg',
+            imgLink2H: 'huguenin-twitter.jpg',
+            imgLink3H: 'huguenin-insta.jpg',            
+            colorBackgroundH: ''
+          },
+          {
+            logoK: 'assets/images/kpsule-logo.png',
+            descriptionK: 'HUGUENIN C2I Spécialiste de l\'immobilier en Ile-de-france'
+          }        
+        ]                     
+      }
+    }
 
 
+  })();
